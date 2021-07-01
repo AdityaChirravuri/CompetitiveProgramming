1ed6ede84adc5029742a140fb514211feed44cf5
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-07-01 09:37:04 +0530

    Create FindingSquareRoots.java

---
+++ Codechef/Practice/Beginner/FindingSquareRoots.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t>0){
+		    int num = sc.nextInt();
+		    
+		    System.out.println((int)Math.sqrt(num));
+		    t--;
+		}
+	}
+}
