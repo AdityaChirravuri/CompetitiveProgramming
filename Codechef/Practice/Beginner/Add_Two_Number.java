1c3dff87560a43a0a847dd5db4ce76952887540c
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-07-01 09:30:33 +0530

    Create Add_Two_Number.java

---
+++ Codechef/Practice/Beginner/Add_Two_Number.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    long testcases = sc.nextLong();
+	    
+	    while(testcases>0){
+	        long a = sc.nextLong();
+	        long b = sc.nextLong();
+	        
+	        System.out.println(a+b);
+	        
+	        testcases--;
+	    }
+	    
+	}
+}
