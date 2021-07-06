7525f1d6cacac52a03a419ffb57a378df1b01721
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-07-06 07:51:01 +0530

    Create Zoos.c

---
+++ HackerEarth/Basic Programming/Input and Output/Zoos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, j, num_z =0, num_o = 0;
+    char s[100];
+    printf("enter sentence : ");
+    scanf("%s", &s);
+    printf("%s\n", s);
+
+    int l = strlen(s);
+    printf("%d", l);
+    for(i=0; i<l; i++){
+        strcmp(s[i], z)
+    }
+    printf("\n%d %d\n", num_z, num_o);
+
+    int k = num_o - (2 * num_z);
+    if(k==0)
+        printf("YES");
+    else printf("NO");
+}
