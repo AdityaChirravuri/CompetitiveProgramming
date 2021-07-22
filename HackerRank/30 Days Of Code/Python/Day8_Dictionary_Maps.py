e78de52673baff70e24130c62e0e9098b25bcd1f
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-07-22 08:42:45 +0530

    Create Day8_Dictionary_Maps.py

---
+++ HackerRank/30 Days Of Code/Python/Day8_Dictionary_Maps.py
@@ -0,0 +1,17 @@
+phoneBook = dict()
+n = int(input())
+for i in range(n):
+    name, phone_number = input().split(" ")
+    #print(name, phone_number)
+    if i not in phoneBook:
+        phoneBook[name] = phone_number
+    
+while True:
+    try:
+        name = input()
+    except EOFError as e:
+        break    
+    if name in phoneBook:
+            print(name+"="+phoneBook[name])
+    else:
+        print ("Not found")
