677a4ff2c6e3afc27b45ce2fb8741fd88e197c0d
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-07 16:09:25 +0530

    Create SpecialTriplets.cpp

---
+++ Codechef/Long Challenges/August2021/SpecialTriplets.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    int count = 0;
+	    int n;
+	    cin >> n;
+	    for(int c=1; c<=n; c++){
+	        for(int b=c; b<=n; b+=c){
+	            if(b%c == 0){
+	                for(int a=c; a<=n; a+=b){
+	                    if(a%c == 0&&a%b == c)count++;
+	                }
+	            }
+	        }
+	    }
+	    cout <<  count << "\n";
+	}
+	
+	return 0;
+}
