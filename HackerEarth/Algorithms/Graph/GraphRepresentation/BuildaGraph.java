bee23dd2a5d334dfed8226cc158c48148724f5d5
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-18 10:09:53 +0530

    Create BuildaGraph.java

---
+++ HackerEarth/Algorithms/Graph/GraphRepresentation/BuildaGraph.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ 
+ 
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n < 12)System.out.println("No");
+        else System.out.println("Yes");
+    }
+}
