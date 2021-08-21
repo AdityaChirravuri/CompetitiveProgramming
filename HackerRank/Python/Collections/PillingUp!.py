c64f803ef31abf38906163bdadc4d26cb373c69c
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-21 18:25:53 +0530

    Create PillingUp!.py

---
+++ HackerRank/Python/Collections/PillingUp!.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def sol(arr):
+    pile = float('inf')
+    while arr:
+    #num = arr.pop()
+        num =  arr.pop(0) if arr[0] > arr[-1] else arr.pop(-1)
+        
+        if num > pile:
+            return "No"
+        
+        pile = num
+    return "Yes"
+    
+
+for _ in range(int(input())):
+    n = int(input())
+    #arr = deque(list(map(int, input().split())))
+    arr = list(map(int, input().split()))
+    print(sol(arr))
