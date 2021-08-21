d7d3c5d91730d4dccef511d615c5085a19c86ea3
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-21 18:23:53 +0530

    Create Collections_OrderedDict.py

---
+++ HackerRank/Python/Collections/Collections_OrderedDict.py
@@ -0,0 +1,14 @@
+import collections 
+
+l = {}
+n = int(input())
+
+for _ in range(0, n):
+    string, space, k = input().rpartition(' ')
+    k = int(k)
+    if string in l:
+        l[string] += k
+    else: l[string] = k
+    
+for i, j in l.items():
+    print(i, j)
