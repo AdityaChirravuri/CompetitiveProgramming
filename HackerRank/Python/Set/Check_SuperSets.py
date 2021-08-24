67f781b537bb0e1aa97cbbb21cf67940a6c61343
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-24 07:57:34 +0530

    Create Check_SuperSets.py

---
+++ HackerRank/Python/Set/Check_SuperSets.py
@@ -0,0 +1,10 @@
+orginal_Set = list(map(int, input().split()))
+
+count = int(0)
+n = int(input())
+for _ in range(n):
+    sub_Set = list(map(int, input().split()))
+    if (set(sub_Set).issubset(set(orginal_Set))):
+        count += 1
+        
+print(count == n)
