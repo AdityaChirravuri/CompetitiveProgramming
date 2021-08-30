25021364aa2b919d27a677b9eca32ed4753718f4
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-30 07:47:17 +0530

    Create CamelCase.c

---
+++ HackerRank/Problem Solving/Algorithms/Strings/CamelCase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[100];
+    int i, n=1;
+    printf("ENTER STRING: ");
+    scanf("%s", s);
+
+    for(i=0; s[i]!='\0'; i++){
+        if(s[i] >= 'A'&&s[i] <= 'Z'){
+            s[i] = '\n';
+            n++;
+        }
+    }
+    printf("%s", s);
+    printf("\n%d", n);
+    return 0;
+}
